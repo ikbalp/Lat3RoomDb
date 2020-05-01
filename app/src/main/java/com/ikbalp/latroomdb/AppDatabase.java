99315f8f992a529ee7fe02314c5792a2a3574523
ikbalp <63147281+ikbalp@users.noreply.github.com>
2020-05-01 13:50:33 +0700

    pertama

---
+++ app/src/main/java/com/ikbalp/latroomdb/AppDatabase.java
@@ -0,0 +1,15 @@
+package com.ikbalp.latroomdb;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AktivisDao aktivisDao();
+}
+
+
+
+
